e5be30133a158cc93c274b3d470c45cc86761da2
Lisa <lisastozdev@gmail.com>
2014-06-10 18:40:32 +0100

    list ordering

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -2,18 +2,29 @@
 
 /* jasmine specs for controllers go here */
 
-describe('PhoneListCtrl', function(){
+describe('PhoneCat controllers', function() {
 
-  beforeEach(module('phonecatApp'));
+	describe('PhoneListCtrl', function(){
 
-  it('should create "phones" model with 3 phones', inject(function($controller) {
-    var scope = {},
-        ctrl = $controller('PhoneListCtrl', {$scope:scope});
+	  var scope, ctrl;
 
-    expect(scope.phones.length).toBe(3);
-	expect(scope.name).toBe('World');
-  }));
-  
- 
+	  beforeEach(module('phonecatApp'));
+	  
+	  beforeEach(inject(function($controller){
+		scope = {};
+		ctrl = $controller('PhoneListCtrl', {$scope:scope});
+	  
+	  }));
 
+	  it('should create "phones" model with 3 phones', function() {
+		expect(scope.phones.length).toBe(3);
+	  });
+
+
+	  it('should set the default value of orderProp model', function() {
+		expect(scope.orderProp).toBe('age');
+	  });
+
+	});
+	
 });
